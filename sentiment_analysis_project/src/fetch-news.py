261ec5b76c1f578b6a58743849c0562a0347cb50
shivanikakinada <shivanikakinada@gmail.com>
2026-05-31 16:41:23 +0530

    making the topic dynamic

--- sentiment_analysis_project/src/fetch-news.py
+++ sentiment_analysis_project/src/fetch-news.py
@@ -5,12 +5,37 @@ import pandas as pd
 
 load_dotenv()
 
-
 API_KEY = os.getenv("NEWS_API_KEY")
+
+# Available topics
+topics = {
+    1: "artificial intelligence",
+    2: "python",
+    3: "aws",
+    4: "docker",
+    5: "kubernetes",
+    6: "cybersecurity",
+    7: "machine learning",
+    8: "data science"
+}
+
+print("\nAvailable Topics:")
+for num, topic in topics.items():
+    print(f"{num}. {topic}")
+
+choice = int(input("\nSelect a topic number: "))
+
+if choice not in topics:
+    print("Invalid choice!")
+    exit()
+
+selected_topic = topics[choice]
+
+print(f"\nFetching news for: {selected_topic}")
 
 url = (
     f"https://newsapi.org/v2/everything?"
-    f"q=artificial intelligence&"
+    f"q={selected_topic}&"
     f"language=en&"
     f"sortBy=publishedAt&"
     f"apiKey={API_KEY}"
@@ -18,25 +43,30 @@ url = (
 
 response = requests.get(url)
 
-print(response.status_code)
+if response.status_code != 200:
+    print("Failed to fetch news")
+    print(response.text)
+    exit()
+
 data = response.json()
-print (data.keys()) #printing only keys in the response :output is status,totalresults,articles
 
-articles= data["articles"]
-rows = [] # initialising with empty rows
+rows = []
 
-for article in articles: #appending required columns
-        rows.append({
+for article in data["articles"]:
+    rows.append({
+        "source": article["source"]["name"],
         "title": article["title"],
         "description": article["description"],
         "published_at": article["publishedAt"]
     })
-        
+
 df = pd.DataFrame(rows)
 
-df.to_csv(                    #converting dataframe to csv and storing in a folder
-    "data/news.csv",
+filename = selected_topic.replace(" ", "_") + "_news.csv"
+
+df.to_csv(
+    f"data/{filename}",
     index=False
 )
 
-print("Saved Successfully")
+print(f"\nSaved {len(df)} articles to data/{filename}")
